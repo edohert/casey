36418a67a09e5b6142d88e24ad4ddc9f66ff89bd
Emily Doherty <edohert@gmail.com>
2014-09-28 17:48:26 -0700

    added photos/updated everything

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-    //painting not w/in hover fade
-    $("#paintingPageContent, #digitalArtPageContent, #sketchesPageContent, #brandIDPageContent").children().hover(function() {
-        $(this).siblings().stop().fadeTo(500,0.5);
-    }, function() {
-        $(this).siblings().stop().fadeTo(500,1);
-        });
 
    // changes the title box (currenly not working)
     $(".fancybox").fancybox();
@@ -16,15 +10,14 @@ $(document).ready(function(){
         }
     });     
 
-    //removes the white outline (currently working)
+//removes the white outline (currently working)
     $(".fancybox")
         .attr('rel', 'gallery')
         .fancybox({
             padding : 0
         });
 
-    //search function
-    
+//search function for paintings
     $("#paintingSearch").keyup(function () {
         // Retrieve the input field text and reset the count to zero
         var filter = $(this).val(),
@@ -33,10 +26,12 @@ $(document).ready(function(){
         $("#paintingPageContent .paintings").each(function () {
             // If the list item does not contain the text phrase fade it out
             if ($(this).attr("id").search(new RegExp(filter, "i")) < 0) {
-                $(this).fadeOut();
+                $(this).fadeOut("slow");
+                $(".horizontalLine").hide("slow");
                 // Show the list item if the phrase matches and increase the count by 1
             } else {
                 $(this).show();
+                $(".horizontalLine").show();
                 count++;
             }
         });
@@ -51,12 +46,80 @@ $(document).ready(function(){
         }
     });
 
+//search function for brandID
+    $("#brandIDSearch").keyup(function () {
+        // Retrieve the input field text and reset the count to zero
+        var filter = $(this).val(),
+            count = 0;
+        // Loop through the menu list
+        $("#brandIDPageContent .brandID").each(function () {
+            // If the list item does not contain the text phrase fade it out
+            if ($(this).attr("id").search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut("slow");
+                $(".horizontalLine").hide("slow");
+                // Show the list item if the phrase matches and increase the count by 1
+            } else {
+                $(this).show();
+                $(".horizontalLine").show();
+                count++;
+            }
+        });
+        // Update the count
+        if (count > 0) {
+            $("#filter-count").text("Top " + count + " results for: " + filter);
+        } else {
+            $("#filter-count").text("No results for: " + filter);
+        }
+        if (filter == "") {
+            $("#filter-count").text("")
+        }
+    });
+
+//search function for DigitalArt
+    $("#digitalArtSearch").keyup(function () {
+        // Retrieve the input field text and reset the count to zero
+        var filter = $(this).val(),
+            count = 0;
+        // Loop through the menu list
+        $("#digitalArtPageContent .digitalArt").each(function () {
+            // If the list item does not contain the text phrase fade it out
+            if ($(this).attr("id").search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut("slow");
+                $(".horizontalLine").hide("slow");
+                // Show the list item if the phrase matches and increase the count by 1
+            } else {
+                $(this).show();
+                $(".horizontalLine").show();
+                count++;
+            }
+        });
+        // Update the count
+        if (count > 0) {
+            $("#filter-count").text("Top " + count + " results for: " + filter);
+        } else {
+            $("#filter-count").text("No results for: " + filter);
+        }
+        if (filter == "") {
+            $("#filter-count").text("")
+        }
+    });
+
+// isotope
+    var $wrapper = $("#wrapper");
+    // init
+    $wrapper.isotope({
+        // options
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+    });
+
+//images outside hover fade
+    $("#paintingPageContent, #digitalArtPageContent, #sketchesPageContent, #brandIDPageContent").children().hover(function() {
+        $(this).siblings().stop().fadeTo(500,0.5);
+    }, function() {
+        $(this).siblings().stop().fadeTo(500,1);
+        });
+
 })
 
- // Twitter button   
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],
-    p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){
-    js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}
-    (document, 'script', 'twitter-wjs');
-
-    
+ 
